ebaf94105f88a79dbe129b680df8a9d6d78f398d
Carlos Oliveira <candreoliveira@gmail.com>
2015-06-14 18:27:57 -0300

    feat(map): Generates map and rewrite css.

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-// VENDOR DEPENDENCIES
-var glob = require('glob');
-var fs = require('fs');
+var endTimer = timer();
+
+//
+// VENDOR
+//
+
+var B = require('bases');
 var R = require('ramda');
 var S = require('spots');
 
+//
 // DEPENDENCIES
+//
+
+var F = require('./src/file.js')();
 var H = require('./src/helpers.js')(R);
 var css = require('./src/css')(R);
-var reader = require('./src/reader.js');
 
-// CONFIGS
+//
+// CONFIG FILES
+//
+
 var config = require('./test/config/config.json') || {};
-var ids = [];
-var classes = [];
 
-// CALLBACKS
-var processLineCss = R.curry(function(callback, file){
-  var readline = reader(file);
+//
+// AUXILIARY FUNCTIONS
+//
+
+var error = R.ifElse(R.isNil, R.identity, H.error);
+
+function timer() {
+  var name = '[COMPREcssOR] Execution time';
+  console.time(name);
+  return function() {
+    console.timeEnd(name);
+  }
+}
+
+//P processCss : Fn, String => Void
+//  Reads file to extract ids and classes and execute callback with
+var processCss = R.curry(function(callback, file){
+  var ids = [];
+  var classes = [];
+  var readline = F.readStream(file);
 
   readline.on('data', function(line){
     var t = css.tokens(line+'');
@@ -25,32 +50,111 @@ var processLineCss = R.curry(function(callback, file){
   });
 
   readline.on('end', function(){
-    callback(ids, classes);
+    callback(ids, classes, file);
   });
 });
 
-var error = R.compose(
-  R.ifElse(R.isNil, R.identity, H.error)
-);
+var regenerateCss = R.curry(function(config, file, map) {
+  var prefix = R.compose(
+    H.orElse(''),
+    H.of(
+      R.compose(
+        R.ifElse(
+          R.isEmpty,
+            R.always(''),
+            S(R.concat, S, '\.')),
+        R.propOr('', 'prefix'))),
+    H.Maybe,
+    R.prop('output'));
 
-var callbackCss = R.curry(function(err, files){
-  error(err);
+  var suffix = R.compose(
+    H.orElse('\.css'),
+    H.of(
+      R.compose(
+        R.ifElse(
+          R.isEmpty,
+            R.always(''),
+            R.compose(
+              R.concat('\.'),
+              S(R.concat, S, '\.css'))),
+        R.propOr('', 'suffix'))),
+    H.Maybe,
+    R.prop('output'));
 
-  return R.compose(
-    R.forEach(processLineCss(function(ids, classes){
-      H.log(ids);
-      H.log(classes);
-    }))
-  )(files);
+  var fileName = R.compose(
+    R.concat(prefix(config)),
+    R.replace('\.css', suffix(config)))(file);
+
+  var replaceTokens = R.replace(new RegExp(R.keys(map).join('|'), 'g'), function(str) {
+    return map[str] ? map[str] : str;
+  });
+
+  var read = F.readStream(file);
+  var write = F.writeStream(fileName);
+
+
+  // PREFIX NOT WORKING AND REGEX IS NOT APPROPRIATE
+  read.pipe(F.transformStream(replaceTokens, {objectMode: true})).pipe(write);
 });
 
-var execCss = R.compose(
-  R.forEach(S(glob, S, {}, callbackCss)),
-  H.orElse([]),
-  H.of(R.prop('files')),
-  H.Maybe,
-  R.tap(function(x){
-    fs.mkdir('.tmp', error);
-    return x;
-  })
-)(config.css);
+//P generateMap : Object, Array, Array, String => Object
+//  Generate simple classes and ids, rewrite css file and returns map object
+var generateMap = R.curry(function(config, ids, classes, file) {
+  var assocIndexed = R.curry(function(indexSkip, obj, number, index) {
+    obj[number] = B.toBase64(index + indexSkip);
+    return obj;
+  });
+
+  var mapOutput = R.curry(function(config, output) {
+    R.compose(
+      H.of(S(F.outputJson, S, output, {}, error)),
+      H.Maybe,
+      R.prop('mapFile'))(config);
+  });
+
+  var mapIds = R.reduceIndexed(function(acc, elem, idx, list) {
+      return assocIndexed(0, acc, elem, idx);
+    }, {});
+
+  var mapClasses = R.curry(function(classes, output) {
+    var skip = R.compose(R.prop('length'), R.keys)(output);
+
+    return R.reduceIndexed(function(acc, elem, idx, list) {
+      return assocIndexed(skip, acc, elem, idx);
+    }, output)(classes);
+  });
+
+  return R.compose(
+    R.tap(regenerateCss(config, file)),
+    R.tap(mapOutput(config)),
+    mapClasses(classes),
+    mapIds
+  )(ids);
+});
+
+//P callbackCss : Object, Fn, Array => void
+var callbackCss = R.curry(function(config, err, files){
+  error(err);
+  R.compose(R.forEach(processCss(generateMap(config))))(files);
+});
+
+//
+// EXECUTION
+//
+
+var execCss = function(config){
+  return R.compose(
+    endTimer,
+    R.forEach(S(F.glob, S, {}, callbackCss(config))),
+    H.orElse([]),
+    H.of(R.prop('files')),
+    H.Maybe,
+    R.prop('css')
+  )(config);
+}
+
+execCss(config);
+
+module.exports = {
+  generateMap: execCss
+}
---
+++ src/file.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var fse = require('fs-extra');
+var util = require('util');
+var glob = require('glob');
+var stream = require('stream');
+var Transform = stream.Transform;
+
+module.exports = function() {
+  function transformStream(transform, options) {
+    function TransformFile(options) {
+      // allow use without new
+      if (!(this instanceof TransformFile)) {
+        return new TransformFile(options);
+      }
+
+      // init Transform
+      Transform.call(this, options);
+    }
+
+    util.inherits(TransformFile, Transform);
+
+    TransformFile.prototype._transform = function (chunk, enc, cb) {
+      this.push(transform(chunk + ''), enc);
+      cb();
+    };
+
+    return TransformFile(options);
+  }
+
+  return {
+    readStream: fs.createReadStream,
+    writeStream: fs.createWriteStream,
+    transformStream: transformStream,
+    writeFile: fs.writeFile,
+    outputJson: fse.outputJson,
+    glob: glob
+  }
+}
